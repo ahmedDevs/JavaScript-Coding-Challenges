03c665b8b3e0ea961349d3096781a609c717154e
ahmedDevs <ahmed.soft.dev@gmail.com>
2022-05-06 06:23:52 +0300

    i

---
+++ Sum of all arguments/main.js
@@ -0,0 +1,17 @@
+
+/*
+Write a function that finds the sum of all its arguments.
+
+eg:
+
+sum(1, 2, 3) // => 6
+sum(8, 2) // => 10
+sum(1, 2, 3, 4, 5) // => 15
+*/
+
+
+function sum() {
+    let argumentsArray = Array.from(arguments);
+    return argumentsArray.reduce((sum, current) => sum + current, 0);
+}
+
