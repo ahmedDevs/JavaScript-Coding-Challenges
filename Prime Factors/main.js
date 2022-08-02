0a6204a8105c646818b94446dbfe168a9726b227
ahmedDevs <ahmed.soft.dev@gmail.com>
2022-08-01 21:01:06 -0400

    i

---
+++ Prime Factors/main.js
@@ -0,0 +1,31 @@
+/*
+DESCRIPTION:
+Prime Factors
+Inspired by one of Uncle Bob's TDD Kata
+
+Write a function that generates factors for a given number.
+
+The function takes an integer on the standard input and returns a list of integers 
+(ObjC: array of NSNumbers representing integers). That list contains the prime factors 
+in numerical sequence.
+
+Examples
+1  ==>  []
+3  ==>  [3]
+8  ==>  [2, 2, 2]
+9  ==>  [3, 3]
+12 ==>  [2, 2, 3]
+*/
+
+
+
+function prime_factors(n) {
+    const primes = []
+    for(let i = 2; i <= n; i++) {
+        while((n % i) === 0) {
+            primes.push(i)
+            n /= i
+        } 
+    }
+    return primes
+}
