750fa1017facfba163c92784387bb77eb73296ea
ahmedDevs <ahmed.soft.dev@gmail.com>
2022-12-21 18:16:03 -0500

    i

--- Fibonacci Number/main.js
+++ Fibonacci Number/main.js
@@ -39,7 +39,7 @@ Constraints:
     if(n === 0) return 0
     const arr = [0,1]
     for(let i = 2; i <= n; i++) {
-        arr.push(arr[i - 1] + arr[i - 2])
+        arr[i] = arr[i - 1] + arr[i - 2]
     }
     return arr[arr.length - 1]
 }
