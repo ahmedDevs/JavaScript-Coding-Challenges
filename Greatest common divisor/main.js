b7221955c6d2dbb28ea763d306150ceab3f74fe9
ahmedDevs <ahmed.soft.dev@gmail.com>
2023-01-31 17:34:54 -0500

    Recursive method using euclidian algorithm

--- Greatest common divisor/main.js
+++ Greatest common divisor/main.js
@@ -7,29 +7,57 @@ The inputs x and y are always greater or equal to 1, so the greatest common
  */
 
 
- function divisor(x,y) {
-    if(x > y) {
-        let remainder = x % y;
-        x = remainder
-        while(x !== 0) {
-            if (x / y === 0) {
-                break;
-            }
-            x = x/y;
+// for loop method
+// function gcd (x,y) {
+//   const greaterNum = Math.max(x,y)
+//   for(let i = greaterNum; i > 0; i--) {
+//     if(x % i === 0 && y % i === 0) {
+//         return i
+//     }
+//   }
+// }
+
+
+// recursive method
+// euclidian algorithm
+function gcd (x,y) {
+    return !y ? x : gcd(y, x % y)
+}
+
+
+
+
+
+
+
+//  function divisor(x,y) {
+//     if(x > y) {
+//         let remainder = x % y;
+//         x = remainder
+//         while(x !== 0) {
+//             if (x / y === 0) {
+//                 break;
+//             }
+//             x = x/y;
            
-        } 
-        return x;
+//         } 
+//         return x;
 
-    }  else if (x < y) {
-         let remainder = y % x;
-         while(remainder !== 0) {
-            if(remainder % x === 0) {
-                break;
-            }
-             remainder = remainder % x;
+//     }  else if (x < y) {
+//          let remainder = y % x;
+//          while(remainder !== 0) {
+//             if(remainder % x === 0) {
+//                 break;
+//             }
+//              remainder = remainder % x;
             
-         }
-         return remainder;
-    }
+//          }
+//          return remainder;
+//     }
 
- }
+//  }
+
+ // test cases
+console.log(gcd(30,12),6)
+console.log(gcd(8,9),1)
+console.log(gcd(1,1),1)
